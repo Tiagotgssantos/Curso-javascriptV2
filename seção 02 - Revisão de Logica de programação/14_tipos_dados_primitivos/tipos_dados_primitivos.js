cff6eeb5aa8cf80f2164ad386f5a3685effb2f16
Tiago dos Santos <tiagotgssantostrade@gmail.com>
2023-08-11 19:55:27 -0300

    aulas de logica de programação

---
+++ seção 02 - Revisão de Logica de programação/14_tipos_dados_primitivos/tipos_dados_primitivos.js
@@ -0,0 +1,6 @@
+//strings, number (int, floats), boolean
+
+
+//Caso eu queira que as aspas apareça no código eu posso escrever da seguinte forma:
+let frase='Minha "string" com aspas duplas.';
+console.log(frase);
---
+++ seção 02 - Revisão de Logica de programação/15_number/number.js
@@ -0,0 +1,8 @@
+let nome='Tiago';
+let idade = 32;
+let msg = `Meu nome é ${nome} e eu possuo ${idade} anos`;
+
+console.log(msg);
+console.log(typeof nome, typeof idade, typeof msg);
+/*O operador typeof em JavaScript é usado para determinar
+ o tipo de dado de uma expressão ou variável. */
