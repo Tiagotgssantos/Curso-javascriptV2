1e2becfb4816b170f65f7e169d9b330b14fb207e
Tiago dos Santos <tiagotgssantostrade@gmail.com>
2023-08-15 17:54:42 -0300

    ate a aula 27

---
+++ seção 02 - Revisão de Logica de programação/18_19_conversão_entre_tipos/conversão_entre_tipos.js
@@ -0,0 +1,9 @@
+let n1 = "10.234445";
+n1 = parseFloat(n1);
+// convertendo o n1 para um número float
+
+let n2 = 2;
+
+console.log(n1 + n2, typeof n1, typeof n2);
+/*O operador typeof em JavaScript é usado para determinar
+ o tipo de dado de uma expressão ou variável. */ 
